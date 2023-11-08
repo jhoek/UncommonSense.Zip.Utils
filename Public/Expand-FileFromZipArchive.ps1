20d9cf80089ceae572e5906959006ac400da7623
Jan Hoek <jhoek@uncommonsense.nl>
2023-11-08 20:17:13 +0100

    More preparations

---
+++ Public/Expand-FileFromZipArchive.ps1
@@ -0,0 +1,4 @@
+function Expand-FileFromZipArchive
+{
+
+}
--- UncommonSense.Zip.Utils.tests.ps1
+++ UncommonSense.Zip.Utils.tests.ps1
@@ -1,7 +1,16 @@
-$CentralDirectory = Get-Content -Path ~/Desktop/test.zip -AsByteStream
-| Select-Object -Last 22
-| Get-ZipEndOfCentralDirectoryInfo
+Describe 'UncommonSense.Zip.Utils' {
+    Context 'From a url' {
+        It 'Successfully extracts a file' {}
+    }
+    Context 'From a file' {
+        It 'Successfully extracts a file' {}
+    }
+}
 
-Get-Content -Path ~/Desktop/test.zip -AsByteStream
-| Select-Object -Skip $CentralDirectory.Offset -First $CentralDirectory.Size
-| Get-ZipCentralDirectoryInfo
+# $CentralDirectory = Get-Content -Path ~/Desktop/test.zip -AsByteStream
+# | Select-Object -Last 22
+# | Get-ZipEndOfCentralDirectoryInfo
+
+# Get-Content -Path ~/Desktop/test.zip -AsByteStream
+# | Select-Object -Skip $CentralDirectory.Offset -First $CentralDirectory.Size
+# | Get-ZipCentralDirectoryInfo
