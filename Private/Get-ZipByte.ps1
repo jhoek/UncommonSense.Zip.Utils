27c74273703fceacf9b88bb47bd3a0a36a1e4b22
Jan Hoek <jhoek@uncommonsense.nl>
2023-11-11 12:23:05 +0100

    More preparations

--- Private/Get-ZipByte.ps1
+++ Private/Get-ZipByte.ps1
@@ -1,18 +1,19 @@
 function Get-ZipByte
 {
+    [CmdletBinding(DefaultParameterSetName = 'Path')]
     [OutputType([byte])]
     param
     (
-        [Parameter(Mandatory, ParameterSetName = 'Uri')]
+        [Parameter(Mandatory, ParameterSetName = 'Uri', Position = 0)]
         [string]$Uri,
 
-        [Parameter(Mandatory, ParameterSetName = 'Path')]
+        [Parameter(Mandatory, ParameterSetName = 'Path', Position = 0)]
         [string]$Path,
 
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, Position = 1)]
         [int]$Offset,
 
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, Position = 2)]
         [int]$Size
     )
 
@@ -20,12 +21,18 @@ function Get-ZipByte
     {
         'Uri'
         {
-            Invoke-WebRequest -Uri $Uri
+            Invoke-WebRequest `
+                -Uri $Uri `
+                -Headers @{'Range' = "bytes=$($Offset)-$($Offset + $Size - 1)" }
+            | Select-Object -ExpandProperty Content
         }
 
         'Path'
         {
-
+            Get-Content `
+                -Path $Path `
+                -AsByteStream
+            | Select-Object -Skip $Offset -First $Size
         }
     }
 }
--- UncommonSense.Zip.Utils.tests.ps1
+++ UncommonSense.Zip.Utils.tests.ps1
@@ -1,11 +1,16 @@
-Describe 'UncommonSense.Zip.Utils' {
-    Context 'From a url' {
-        It 'Successfully extracts a file' {}
-    }
-    Context 'From a file' {
-        It 'Successfully extracts a file' {}
-    }
-}
+. ./Private/Get-ZipByte.ps1
+
+Get-ZipByte ~/Dropbox/test.zip 0 10
+Get-ZipByte -Uri 'https://www.dropbox.com/scl/fi/73eei6vciw7e73x3vzb3m/test.zip?rlkey=xket6dhbe2qd1fnfh1yc13ni9&dl=1' 0 10
+
+# Describe 'UncommonSense.Zip.Utils' {
+#     Context 'From a url' {
+#         It 'Successfully extracts a file' {}
+#     }
+#     Context 'From a file' {
+#         It 'Successfully extracts a file' {}
+#     }
+# }
 
 # $CentralDirectory = Get-Content -Path ~/Desktop/test.zip -AsByteStream
 # | Select-Object -Last 22
